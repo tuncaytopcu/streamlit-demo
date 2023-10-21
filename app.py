66b86168acf06cade8f948ff03cf8d0768a29d5d
Yunus Emre Gündoğmuş <yemregun@icloud.com>
2023-10-22 01:18:42 +0300

    added base_code

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from sumapi.api import SumAPI
+
+api = SumAPI(username='kave', password='kave')
+
+def get_sentiment(text: str):
+    result = api.sentiment_analysis(text, domain='general')
+    return result['evaluation']['label']
+
+def get_color(sentiment):
+    # Duygu analizine göre renk döndür
+    if sentiment == "Pozitif":
+        return "#A8E6CF"  # Pastel Yeşil
+    elif sentiment == "Negatif":
+        return "#FF8A80"  # Pastel Kırmızı
+    elif sentiment == "Nötr":
+        return "#FFD3B6"  # Pastel Turuncu
+
+st.title("SumAPI Duygu Analizi Uygulaması")
+
+user_input = st.text_input("Bir şeyler yazın:")
+
+if user_input:
+    sentiment = get_sentiment(user_input)
+    color = get_color(sentiment)
+    st.write(f'<div style="color: {color};">Duygu Analizi Sonucu: {sentiment}</div>', unsafe_allow_html=True)
